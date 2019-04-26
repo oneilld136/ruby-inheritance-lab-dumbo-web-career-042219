146c338d326a3af29bf44ab022a1aac670489ebe
Danielle O'Neill <oneilld136@gmail.com>
2019-04-26 13:50:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+  def initialize(knowledge)
+    @knowledge=[]
+  end
+
+  def learn(string)
+    @knowledge << "string"
+  end
+
+  def knowledge
+    return @knowledge
+end
 end
---
+++ lib/user.rb
@@ -0,0 +1,15 @@
+class User
+
+attr_accessor :first_name,  :last_name
+
+  def initialize(first_name=nil,last_name=nil) #NIL
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def teach
+    student_array=
+    student_array.sample
+
+  end
+end
